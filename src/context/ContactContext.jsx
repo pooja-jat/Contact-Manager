f68eec89130b6ebb419e585b86ac6cdd32da54e6
Pooja jat <patelpoojajat@gmail.com>
2025-03-28 22:58:49 +0530

    fixed

--- src/context/ContactContext.jsx
+++ src/context/ContactContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useReducer } from "react";
-import { ContactReducer } from "./contactReducer";
+import { ContactReducer } from "./ContactReducer";
 
 const ContactContext = createContext();
 
